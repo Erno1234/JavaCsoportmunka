08c5edb17a831396a5adec4e80105f60e14274d9
Erno1234 <palocierno12>
2023-11-19 20:51:57 +0100

    View fájl importok

--- src/View/Kolcsonzes.java
+++ src/View/Kolcsonzes.java
@@ -1,6 +1,17 @@
-
 package View;
 
+import Modell.EKonyv;
+import Modell.Konyvtar;
+import Modell.PapirKonyv;
+
 public class Kolcsonzes {
-    
+
+    private final Konyvtar konyvtar;
+
+    public Kolcsonzes() {
+        konyvtar = new Konyvtar(4);
+        konyvtar.kolcsonzottKonyvek(new PapirKonyv("János",""));
+        konyvtar.kolcsonzottKonyvek(new EKonyv("Petra",""));
+    }
+
 }
